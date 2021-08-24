94a8036b1c6bd8e198ce48102d067f4b8ea937bc
薛斌 <928691436@qq.com>
2021-08-25 00:49:26 +0800

    html结构搭建

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div>导航栏 | <router-link to="/" >Ben</router-link> | <router-link to="/xxx" >Ben2</router-link>
-  </div>
-  <hr>
   <router-view></router-view>
 </template>
 
 <script>
-import Ben from './components/Ben.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Ben
-  }
-}
+  name: "App",
+};
 </script>
+
+
+<style lang="scss" scoped>
+.topnav {
+  border: 1px solid red;
+}
+</style>
