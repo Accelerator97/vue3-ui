27ad72e28c22f18497c6cf86b48383b77884a049
薛斌 <928691436@qq.com>
2021-08-24 22:25:12 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Ben/>
+</template>
+
+<script>
+import Ben from './components/Ben.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Ben
+  }
+}
+</script>
