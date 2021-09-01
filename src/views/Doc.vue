6030a298f3d24a0fd07972d59f8a180b63aa190b
薛斌 <928691436@qq.com>
2021-09-01 18:36:37 +0800

    初始化aside

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,18 +1,33 @@
 <template>
-    <div>
-        <Topnav/>
-        <div class="content">
-            <aside>边栏</aside>
-            <main>主内容</main>
-        </div>
+  <div>
+    <Topnav />
+    <div class="content">
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">button组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/diaLog">Diaslog组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs组件</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>主内容</main>
     </div>
+  </div>
 </template>
 
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue";
 export default {
-    components:{Topnav}
-    
-}
+  components: { Topnav },
+};
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,9 @@
         <div class="banner">
             <h1>咕噜UI</h1>
             <h2>一个简易的UI框架</h2>
-            <p class="action">
-                <a href="">Github</a>
-                <a href="">开始</a>
+            <p class="actions">
+                <a href="https://github.com">Github</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
@@ -20,6 +20,25 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 20px 0;
+      a {
+      margin:0 8px;
+      background:#fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
