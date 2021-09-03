fb7a415c5f047e4a7641e2f9718f9696fc7cc7c0
薛斌 <928691436@qq.com>
2021-09-03 13:43:21 +0800

    按钮切换动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <button><span></span></button>
+    <button @click="toggle" :class="{checked}"><span></span></button>
   </div>
 </template>
+
+
+<script>
+import {ref} from 'vue'
+export default {
+    setup(){
+        const checked = ref(false)
+        const toggle = () => {
+            checked.value = !checked.value
+        }
+        return {checked,toggle}
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 $h: 22px;
@@ -11,7 +25,7 @@ button {
   height: $h;
   width: $h * 2 ;
   border: none;
-  background: blue;
+  background:grey;
   border-radius: $h/2;
   position: relative;
 }
@@ -23,8 +37,12 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2/2;
+  transition: left 250ms;
 }
-button:hover > span{
+button.checked{
+    background: blue;
+}
+button.checked > span{
     left: calc(100% - #{$h2} - 2px)
 }
 </style>
